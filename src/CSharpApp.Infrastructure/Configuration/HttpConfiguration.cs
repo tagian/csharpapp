70a97830ff16c4bb315c6112524265f23764a34e
tagian <61740503+tagian@users.noreply.github.com>
2026-09-24 00:34:42 +0000

    Added resilience and connection lifetime on Http Client

--- src/CSharpApp.Infrastructure/Configuration/HttpConfiguration.cs
+++ src/CSharpApp.Infrastructure/Configuration/HttpConfiguration.cs
@@ -1,15 +1,67 @@
-namespace CSharpApp.Infrastructure.Configuration;
+// namespace CSharpApp.Infrastructure.Configuration;
+
+// public static class HttpConfiguration
+// {
+//     public static IServiceCollection AddHttpConfiguration(
+//         this IServiceCollection services,)
+//     {
+//         services.AddHttpClient("PlatziFakeStore", client =>
+//         {
+//             client.BaseAddress = new Uri("https://api.escuelajs.co/api/v1/");
+//                             new Uri(configuration["UpstreamApi:BaseUrl"]!);
+
+//         });
+
+//         return services;
+//     }
+
+// }
+
 
 public static class HttpConfiguration
 {
-    public static IServiceCollection AddHttpConfiguration(this IServiceCollection services)
+    public static IServiceCollection AddHttpConfiguration(
+        this IServiceCollection services)
     {
-        services.AddHttpClient("PlatziFakeStore", client =>
-        {
-            client.BaseAddress = new Uri("https://api.escuelajs.co/api/v1/");
-        });
+        services
+            .AddHttpClient("PlatziFakeStore", (serviceProvider, client) =>
+            {
+                var restApiSettings = serviceProvider
+                    .GetRequiredService<IOptions<RestApiSettings>>()
+                    .Value;
+
+                var httpClientSettings = serviceProvider
+                    .GetRequiredService<IOptions<HttpClientSettings>>()
+                    .Value;
+
+                client.BaseAddress = new Uri(restApiSettings.BaseUrl!);
+            })
+            .ConfigurePrimaryHttpMessageHandler(serviceProvider =>
+            {
+                var settings = serviceProvider
+                    .GetRequiredService<IOptions<HttpClientSettings>>()
+                    .Value;
+
+                return new SocketsHttpHandler
+                {
+                    PooledConnectionLifetime = TimeSpan.FromMinutes(settings.LifeTime)
+                };
+            })
+            .AddResilienceHandler("retry", (pipeline, context) =>
+            {
+                var settings = context.ServiceProvider
+                    .GetRequiredService<IOptions<HttpClientSettings>>()
+                    .Value;
+
+                pipeline.AddRetry(new HttpRetryStrategyOptions
+                {
+                    MaxRetryAttempts = settings.RetryCount,
+                    Delay = TimeSpan.FromMilliseconds(settings.SleepDuration),
+                    BackoffType = DelayBackoffType.Constant,
+                    UseJitter = false
+                });
+            });
 
         return services;
     }
-
 }
--- src/CSharpApp.Infrastructure/GlobalUsings.cs
+++ src/CSharpApp.Infrastructure/GlobalUsings.cs
@@ -5,3 +5,6 @@ global using CSharpApp.Core.Interfaces;
 global using CSharpApp.Core.Settings;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Http.Resilience;
+global using Microsoft.Extensions.Options;
+global using Polly;
